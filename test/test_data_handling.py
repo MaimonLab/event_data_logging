e3f611c9cb03e0a465455a4f70d50530b440d29d
tlmohren <tlmohren@nogmail.com>
2022-08-26 14:28:38 -0400

    fix bug in unpacking dict header, voltages_voltage_0 to voltages_0_voltage

--- test/test_data_handling.py
+++ test/test_data_handling.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 import numpy as np
 import pytest
-
 from event_data_logging.data_handling import flatten_dictionary
 
 
@@ -16,6 +15,8 @@ def test_flatten_dictionary():
             {"voltage": 0.2},
             {"voltage": 3.1},
         ],
+        "single_fly": [{"x": 1.0, "y": 2.0, "r": 0.2}],
+        "flies": [{"x": 1.0, "y": 2.0, "r": 0.2}, {"x": 4.0, "y": 5.0, "r": 0.4}],
         "amps": np.array([3.2, 1.1]),
         "testlist": ["a", "b", "c"],
         "username": "John Doe",
@@ -27,8 +28,17 @@ def test_flatten_dictionary():
         "header_timestamp_sec",
         "header_timestamp_nanosec",
         "header_frameID",
-        "voltages_voltage_0",
-        "voltages_voltage_1",
+        "voltages_0_voltage",
+        "voltages_1_voltage",
+        "single_fly_0_x",
+        "single_fly_0_y",
+        "single_fly_0_r",
+        "flies_0_x",
+        "flies_0_y",
+        "flies_0_r",
+        "flies_1_x",
+        "flies_1_y",
+        "flies_1_r",
         "amps_0",
         "amps_1",
         "testlist_0",
@@ -36,10 +46,30 @@ def test_flatten_dictionary():
         "testlist_2",
         "username",
     ]
-    test_data = [3, 123, 3, 0.2, 3.1, 3.2, 1.1, "a", "b", "c", "John Doe"]
+    test_data = [
+        3,
+        123,
+        3,
+        0.2,
+        3.1,
+        1.0,
+        2.0,
+        0.2,
+        1.0,
+        2.0,
+        0.2,
+        4.0,
+        5.0,
+        0.4,
+        3.2,
+        1.1,
+        "a",
+        "b",
+        "c",
+        "John Doe",
+    ]
 
     assert computed_header == test_header
-
     assert computed_data == test_data
 
 
@@ -82,12 +112,14 @@ def test_flatten_ordered_dict():
         "header_timestamp_sec",
         "header_timestamp_nanosec",
         "header_frameID",
-        "voltages_voltage_0",
-        "voltages_voltage_1",
+        "voltages_0_voltage",
+        "voltages_1_voltage",
     ]
     goal_data = [3, 123, 3, 0.2, 3.1]
-    # print(f"{computed_header = }")
-    # print(f"{computed_data = }")
     assert computed_header == goal_header
 
     assert computed_data == goal_data
+
+
+if __name__ == "__main__":
+    test_flatten_dictionary()
