af5deeaf02dc60ccdda492d7e3eed175adb2a9f3
tlmohren <tlmohren@nogmail.com>
2022-12-27 11:36:32 -0500

    v0.1.4

--- src/event_data_logging/__init__.py
+++ src/event_data_logging/__init__.py
@@ -5,7 +5,7 @@ instead of:
   from event_data_logging.json_writer import JSONWriter
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 
 from event_data_logging.json_writer import JSONWriter, StampedJSONWriter, TimestampModes
